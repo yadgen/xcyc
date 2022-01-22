f33ae26b6cbe2c418d034b93d01ddf02a66c2d89
yadgen <yadgen@gmail.com>
2022-01-22 13:46:02 +0800

    coding

---
+++ src/Kernel/ServiceContainer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Xcyc\Kernel;
+
+use Pimple\Container;
+
+class ServiceContainer extends Container
+{
+    protected $providers = [];
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->registerProviders($this->getProviders());
+    }
+
+    public function getProviders()
+    {
+        return array_merge([], $this->providers);
+    }
+
+    public function registerProviders(array $providers)
+    {
+        foreach ($providers as $provider) {
+            parent::register(new $provider());
+        }
+    }
+}
